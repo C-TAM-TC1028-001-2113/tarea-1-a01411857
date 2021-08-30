e566d6e57d5b8d051ca0d34bb525bf4eea2bda8a
a01411857 <89591915+a01411857@users.noreply.github.com>
2021-08-30 00:01:03 -0500

    Add files via upload
    codigo de la levadura editado y resuelto

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -2,6 +2,13 @@ def main():
     # escribe tu código abajo de esta línea
     pass
 
+gramos_harina = float(input("Dame la harina en gramos: "))
+gramos_levadura = (gramos_harina / 1000) * 50
+
+print("Necesita estos gramos de levadura:", gramos_levadura)
+
+
+
 
 if __name__ == '__main__':
     main()
