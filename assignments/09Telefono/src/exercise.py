f945bb322400d6a915806f1454eb2dd152f4dd2a
a01411857 <89591915+a01411857@users.noreply.github.com>
2021-08-29 22:12:59 -0500

    Add files via upload
    codigo de telefono editado

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -3,5 +3,15 @@ def main():
     pass
 
 
+num_mensajes = float(input("Dame el numero de mensajes: "))
+num_megas = float(input("Dame el numero de megas: "))
+num_minutos = float(input("Dame el numero de minutos: "))
+
+costo_mensual = (num_mensajes + num_megas + num_minutos) * .8
+
+print("Tu costo mensual es de", costo_mensual)
+
+
+
 if __name__ == '__main__':
     main()
