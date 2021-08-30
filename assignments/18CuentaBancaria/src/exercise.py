ac87b9124830470a9df520e4b94ab991ceacaaa3
a01411857 <89591915+a01411857@users.noreply.github.com>
2021-08-29 21:49:36 -0500

    Add files via upload
    ejercicio de cuenta bancaria editado

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -3,5 +3,17 @@ def main():
     pass
 
 
+saldo_anterior = float(input("Dame el saldo del mes anterior: "))
+ingresos = float(input("Dame el valor de los ingresos: "))
+egresos = float(input("Dame el valor de los egresos: "))
+num_cheques = float(input("Dame el numero de cheques: "))
+
+saldo = (saldo_anterior + ingresos - egresos - num_cheques * 13)
+saldo_final = saldo - (saldo * 0.075)
+
+
+print("El saldo final es", saldo_final)
+
+
 if __name__ == '__main__':
     main()
