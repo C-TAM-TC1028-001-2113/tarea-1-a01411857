50a68aa46106bbaeb82f0f203fdfe42ef23ac018
a01411857 <89591915+a01411857@users.noreply.github.com>
2021-08-29 23:55:39 -0500

    Add files via upload
    codigo resulto y editado

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+import math
+num_palabras = int(input("Dame el numero de palabras: "))
+num_paginas = math.ceil(num_palabras / 475)
+
+num_total = (num_paginas * 60)
+
+costo = 90 * num_total / 100
+
+print("El costo total es de:", costo)
+
 
 
 if __name__ == '__main__':
