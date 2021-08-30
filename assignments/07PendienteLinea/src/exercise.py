97f57c352f8ee518b5bffde8df2ce3ecad6f9bb4
a01411857 <89591915+a01411857@users.noreply.github.com>
2021-08-29 20:48:55 -0500

    Add files via upload
    El trabajo 07 editado

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -3,5 +3,15 @@ def main():
     pass
 
 
+x1 = float(input("Dame x1: "))
+y1 = float(input("Dame y1: "))
+x2 = float(input("Dame x2: "))
+y2 = float(input("Dame y1: "))
+
+m = (y2 - y1) / (x2 - x1)
+
+print("El valor de la pendiente es", m)
+
+
 if __name__ == '__main__':
     main()
