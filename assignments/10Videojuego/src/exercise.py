a36ef4cfeaeadb39a7a0eb21640aeaf221558781
Jose Miguel Gonzalez <a01411857@itesm.mx>
2021-09-04 01:05:28 -0500

    estos son los buenos profe

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -4,7 +4,7 @@ def main():
     juegosnuevos = int(input('Dame la cantidad de juegos nuevos: '))
     juegos_usados = int(input('Dame la cantidad de juegos usados: '))
     total = (juegosnuevos * 1000) + (juegos_usados * 350)
-    print('El total de tu compra es de:', total)
+    print('El total de la compra es :', total)
 
 
 if __name__ == '__main__':
