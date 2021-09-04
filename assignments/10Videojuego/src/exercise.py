3b029a01fa4051ae72d2a49a16d7987ef6825cdf
Jose Miguel Gonzalez <a01411857@itesm.mx>
2021-09-04 01:01:17 -0500

    estos son los buenos profe

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,10 +1,10 @@
 def main():
     # escribe tu código abajo de esta línea
 
-    juegosnuevos = int(input("Dame la cantidad de juegos nuevos: "))
-    juegos_usados = int(input("Dame la cantidad de juegos usados: "))
+    juegosnuevos = int(input('Dame la cantidad de juegos nuevos: '))
+    juegos_usados = int(input('Dame la cantidad de juegos usados: '))
     total = (juegosnuevos * 1000) + (juegos_usados * 350)
-    print("El total de tu compra es de:", total)
+    print('El total de tu compra es de:', total)
 
 
 if __name__ == '__main__':
--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -3,12 +3,10 @@ def main():
 
     import math
     num_palabras = int(input("Dame el numero de palabras: "))
-    num_paginas = math.ceil(num_palabras / 475)
-    num_total = (num_paginas * 60)
+    numpaginas = math.ceil(num_palabras / 475)
+    num_total = (numpaginas * 60)
     precio = 90 * num_total / 100
     print("El costo total es de:", precio)
-
-
 
 if __name__ == '__main__':
     main()
